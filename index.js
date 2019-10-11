07e65109c3b65a3774568df18bb3c62c4d971445
Jean-François Rompré <jrompre@gmail.com>
2019-10-11 20:16:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var store = [];
+  for (let i = 0; i < musicians.length; i++) {
+    store.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return store;
+}
+
+function johnLennonFacts(facts) {
+  let store = [], i = 0;
+
+  while (i < facts.length) {
+    store.push( facts[i] + "!!!")
+    i += 1;
+  }
+  return store;
+}
+
+function iLoveTheBeatles(n) {
+  let store = [], i = n;
+  do {
+    store.push("I love the Beatles!");
+    i++;
+  } while ( (15 - i) > 0);
+  return store;
+}
+
+console.log(iLoveTheBeatles(17))
