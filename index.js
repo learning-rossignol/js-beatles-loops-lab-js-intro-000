5e43c6ec6e4c394ff39b2a77db61e26324c0519b
Jean-François Rompré <jrompre@gmail.com>
2019-10-11 19:50:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var store = [];
+  for (let i = 0; i < musicians.length; i++) {
+    store.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+}
